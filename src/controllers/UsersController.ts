7408392c4a8d4a12d8031dae14ddd487face8464
Pedro Henrique Braga da Silva <pedrohenriquebraga735@gmail.com>
2021-04-24 23:56:38 -0300

    aula03: criar estrutura do Users

---
+++ src/controllers/UsersController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import { UsersService } from "../services/UsersService";
+
+class UsersController {
+
+    async create(req: Request, res: Response): Promise<Response> {
+        const usersService = new UsersService();
+        const { email } = req.body;
+        const user = await usersService.create(email);
+        return res.json(user);
+    }
+}
+
+export { UsersController }
